602919350fed7acbc36a5df76a01d2ddbc2a77ce
Mike Arnos <mikearnos@gmail.com>
2024-09-19 23:56:27 -0400

    add more send commands on/off/code

--- nano_tester/nano_tester.ino
+++ nano_tester/nano_tester.ino
@@ -1,4 +1,4 @@
-/* This will send out the error code 34 */
+/* This will send out ON/OFF, and error codes */
 
 #define LED PD4
 #define SHORT 1
@@ -6,35 +6,59 @@
 
 void setup() {
   pinMode(LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED, LOW);
-  delay(2250);                        // wait for two seconds
+  sendCode(34);
+  sendCode(34);
+  sendCode(34);
+  sendCode(47);
+  sendCode(47);
+  sendCode(47);
 
-  // three short blinks
-  pulse(SHORT);
-  pulse(SHORT);
-  pulse(SHORT);
+  sendHigh(10000);                   // high for ten seconds "ON"
 
-  delay(750);
-
-  // four long blinks
-  pulse(LONG);
-  pulse(LONG);
-  pulse(LONG);
-  pulse(LONG);
+  sendLow(10000);                    // low for ten seconds "OFF"
 }
 
-void pulse(bool short) {
+void sendHigh(int duration) {
   digitalWrite(LED, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(duration);
+}
 
-  if (short) {
-    delay(250);
+void sendLow(int duration) {
+  digitalWrite(LED, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(duration);
+}
+
+void sendCode(int code) {
+  sendLow(2250);                      // begin code is low for 2250ms
+
+  for (int i = code / 10; i > 0; i--) {
+    pulse(SHORT);                     // send tens as short pulses
+  }
+
+  delay(750);                         // pause between pulses
+
+  for (int i = code - ((code / 10) * 10); i > 0; i--) {
+    pulse(LONG);                      // send ones as long pulses
+  }
+}
+
+void pulse(bool shortPulse) {
+  digitalWrite(LED, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  if (shortPulse) {
+    delay(250);                       // short pulse is high 250ms
   } else {
-    delay(1000);
+    delay(1000);                      // long pulse is high 1000ms
   }
 
   digitalWrite(LED, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(250);
 }
